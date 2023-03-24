8a3a8bfe0375385ea7c43a53fd254d06b74b577d
Przemyslaw Wirkus <przemyslaw.wirkus@linaro.org>
2023-03-24 10:54:06 +0000

    Fix for 'Variable 'pmu_device::gpc_nums' is uninitialized'
    See merge request Linaro/WindowsPerf/windowsperf!160
    
    === ChangeLog ===
    
    * wperf: fix for 'Variable 'pmu_device::gpc_nums' is uninitialized'
    
    This is a minor fix for warning:
    ```
    warning C26495: Variable 'pmu_device::gpc_nums' is uninitialized. Always initialize a member variable (type.6).
    ```

--- wperf/pmu_device.cpp
+++ wperf/pmu_device.cpp
@@ -64,6 +64,9 @@ pmu_device::pmu_device() : handle(NULL), count_kernel(false), has_dsu(false), ds
 {
     for (int e = EVT_CLASS_FIRST; e < EVT_CLASS_NUM; e++)
         multiplexings[e] = false;
+
+    memset(gpc_nums, sizeof gpc_nums, 0);
+    memset(fpc_nums, sizeof fpc_nums, 0);
 }
 
 void pmu_device::init(HANDLE hDevice)
