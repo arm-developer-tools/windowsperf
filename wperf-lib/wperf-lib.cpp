a23ccf303a4392265ce3af38aa59e88426aa3dfa
pzhengqc <pzheng@quicinc.com>
2023-05-19 11:03:03 +0000

    [wperf-lib] Add wperf_stat
    See merge request Linaro/WindowsPerf/windowsperf!225
    
    === ChangeLog ===
    
    * [wperf-lib] Add wperf_stat
    
    * The wperf_stat API only supports counting core events. Support for uncore
    events will be added later.
    * Expose pmu_device's `core_outs` and `cores_idx` to help implement the
    wperf_stat API.
    * Set "LanguageStandard" to stdcpp17 for the wperf-lib project.
    * Disable "TreatWChar_tAsBuiltInType" for a few more configurations which were
    missed in !212.

--- wperf-lib/wperf-lib.cpp
+++ wperf-lib/wperf-lib.cpp
@@ -1,11 +1,25 @@
 #include "wperf-lib.h"
+#include "padding.h"
 #include "pmu_device.h"
 #include "wperf-common/public.h"
+#include <regex>
+
+typedef struct _COUNTING_INFO
+{
+    uint64_t counter_value;
+    uint16_t event_idx;
+    uint64_t multiplexed_scheduled;
+    uint64_t multiplexed_round;
+    uint64_t scaled_value;
+    EVENT_NOTE evt_note;
+} COUNTING_INFO;
 
 static pmu_device* __pmu_device = nullptr;
 static struct pmu_device_cfg* __pmu_cfg = nullptr;
 static std::map<enum evt_class, std::vector<uint16_t>> __list_events;
 static size_t __list_index = 0;
+static std::map<enum evt_class, std::vector<struct evt_noted>> __ioctl_events;
+static std::map<uint8_t, std::vector<COUNTING_INFO>> __countings;
 
 extern "C" bool wperf_init()
 {
@@ -35,6 +49,8 @@ extern "C" bool wperf_close()
             delete __pmu_cfg;
 
         __list_events.clear();
+        __ioctl_events.clear();
+        __countings.clear();
     }
     catch(...)
     {
@@ -104,6 +120,203 @@ extern "C" bool wperf_list_events(PLIST_CONF list_conf, PEVENT_INFO einfo)
         {
             // Only Core PMU events are supported for now.
             return false;
+        }
+    }
+    catch (...)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+extern "C" bool wperf_stat(PSTAT_CONF stat_conf, PSTAT_INFO stat_info)
+{
+    static size_t event_index = 0;
+    static size_t core_index = 0;
+
+    try
+    {
+        if (!stat_info)
+        {
+            __ioctl_events.clear();
+            __countings.clear();
+            event_index = 0;
+            core_index = 0;
+
+            std::vector<uint8_t> cores_idx;
+            for (int i = 0; i < stat_conf->num_cores; i++)
+            {
+                cores_idx.push_back(stat_conf->cores[i]);
+            }
+            // Only CORE events are supported at the moment.
+            std::vector<enum evt_class> e_classes = { EVT_CORE };
+            uint32_t enable_bits = __pmu_device->enable_bits(e_classes);
+            __pmu_device->post_init(cores_idx, 0, false, enable_bits);
+
+            uint32_t stop_bits = __pmu_device->stop_bits();
+            __pmu_device->stop(stop_bits);
+
+            // Process events from stat_conf.
+            std::map<enum evt_class, std::deque<struct evt_noted>> events;
+            for (int i = 0; i < stat_conf->num_events; i++)
+            {
+                events[EVT_CORE].push_back({ stat_conf->events[i], EVT_NORMAL, L"e" });
+            }
+
+            // Process groups from stat_conf.
+            std::map<enum evt_class, std::vector<struct evt_noted>> groups;
+            for (int i = 0; i < stat_conf->group_events.num_groups; i++)
+            {
+                groups[EVT_CORE].push_back({ (uint16_t)stat_conf->group_events.num_group_events[i], EVT_HDR, L"" });
+                for (int j = 0; j < stat_conf->group_events.num_group_events[i]; j++)
+                {
+                    groups[EVT_CORE].push_back({ stat_conf->group_events.events[i][j], EVT_GROUPED, L"" });
+                }
+            }
+
+            // Process metrics from stat_conf.
+            std::map<std::wstring, metric_desc>& metrics = __pmu_device->builtin_metrics;
+            for (int i = 0; i < stat_conf->num_metrics; i++)
+            {
+                const wchar_t* metric = stat_conf->metric_events[i];
+                if (metrics.find(metric) == metrics.end())
+                {
+                    // Not a valid builtin metric.
+                    return false;
+                }
+
+                metric_desc desc = metrics[metric];
+                for (const auto& x : desc.events)
+                    events[x.first].insert(events[x.first].end(), x.second.begin(), x.second.end());
+                for (const auto& y : desc.groups)
+                    groups[y.first].insert(groups[y.first].end(), y.second.begin(), y.second.end());
+            }
+
+            set_event_padding(__ioctl_events, *__pmu_cfg, events, groups);
+
+            bool do_kernel = stat_conf->kernel_mode;
+            for (uint32_t core_idx : cores_idx)
+                __pmu_device->events_assign(core_idx, __ioctl_events, do_kernel);
+
+            double count_duration = stat_conf->duration;
+            int64_t counting_duration_iter = count_duration > 0 ?
+                static_cast<int64_t>(count_duration * 10) : _I64_MAX;
+
+            __pmu_device->reset(enable_bits);
+
+            __pmu_device->start(enable_bits);
+
+            int64_t t_count = counting_duration_iter;
+
+            while (t_count > 0)
+            {
+                t_count--;
+                Sleep(100);
+            }
+
+            __pmu_device->stop(enable_bits);
+
+            if (enable_bits & CTL_FLAG_CORE)
+            {
+                __pmu_device->core_events_read();
+                ReadOut* core_outs = __pmu_device->get_core_outs();
+
+                std::vector<uint8_t> counting_cores = __pmu_device->get_cores_idx();
+                for (auto i : counting_cores)
+                {
+                    UINT32 evt_num = core_outs[i].evt_num;
+                    struct pmu_event_usr* evts = core_outs[i].evts;
+                    uint64_t round = core_outs[i].round;
+                    for (UINT32 j = 0; j < evt_num; j++)
+                    {
+                        // Ignore padding events.
+                        if (j >= 1 && (__ioctl_events[EVT_CORE][j - 1].type == EVT_PADDING))
+                            continue;
+
+                        struct pmu_event_usr* evt = &evts[j];
+                        COUNTING_INFO counting_info;
+                        counting_info.counter_value = evt->value;
+                        counting_info.event_idx = evt->event_idx;
+                        if (evt->event_idx == CYCLE_EVT_IDX)
+                        {
+                            counting_info.evt_note.type = NORMAL_EVT_NOTE;
+                        }
+                        else
+                        {
+                            std::wstring note = __ioctl_events[EVT_CORE][j - 1].note;
+                            std::wsmatch m;
+                            if (note == L"e")
+                            {
+                                // This is a NORMAL_EVT_NOTE.
+                                counting_info.evt_note.type = NORMAL_EVT_NOTE;
+                            }
+                            else if (std::regex_match(note, m, std::wregex(L"g([0-9]+),([a-z]+)")))
+                            {
+                                // This is a METRIC_EVT_NOTE.
+                                auto it = metrics.find(m[2]);
+                                if (it == metrics.end())
+                                {
+                                    // Not a valide builtin metric.
+                                    return false;
+                                }
+                                counting_info.evt_note.type = METRIC_EVT_NOTE;
+                                counting_info.evt_note.note.metric_note.group_id = std::stoi(m[1]);
+                                counting_info.evt_note.note.metric_note.name = it->first.c_str();
+                            }
+                            else if (std::regex_match(note, m, std::wregex(L"g([0-9]+)")))
+                            {
+                                // This is a GROUP_EVT_NOTE.
+                                counting_info.evt_note.type = GROUP_EVT_NOTE;
+                                counting_info.evt_note.note.group_note.group_id = std::stoi(m[1]);
+                            }
+                            else
+                            {
+                                // Not a valid event note type.
+                                return false;
+                            }
+                        }
+
+                        counting_info.multiplexed_scheduled = evt->scheduled;
+                        counting_info.multiplexed_round = round;
+                        counting_info.scaled_value = (uint64_t)((double)evt->value / ((double)evt->scheduled / (double)round));
+                        __countings[i].push_back(counting_info);
+                    }
+                }
+            }
+        }
+        else
+        {
+            std::vector<uint8_t> counting_cores = __pmu_device->get_cores_idx();
+            if (core_index >= counting_cores.size())
+            {
+                // No more core to yield.
+                return false;
+            }
+
+            uint8_t core_idx = counting_cores[core_index];
+            if (event_index >= __countings[core_idx].size())
+            {
+                // Start all over for the next core.
+                event_index = 0;
+                core_index++;
+                if (core_index >= counting_cores.size())
+                {
+                    // No more core to yield.
+                    return false;
+                }
+                core_idx = counting_cores[core_index];
+            }
+
+            // Yield next stat_info.
+            stat_info->core_idx = core_idx;
+            stat_info->counter_value = __countings[core_idx][event_index].counter_value;
+            stat_info->event_idx = __countings[core_idx][event_index].event_idx;
+            stat_info->multiplexed_scheduled = __countings[core_idx][event_index].multiplexed_scheduled;
+            stat_info->multiplexed_round = __countings[core_idx][event_index].multiplexed_round;
+            stat_info->scaled_value = __countings[core_idx][event_index].scaled_value;
+            stat_info->evt_note = __countings[core_idx][event_index].evt_note;
+            event_index++;
         }
     }
     catch (...)
--- wperf-lib/wperf-lib.h
+++ wperf-lib/wperf-lib.h
@@ -115,6 +115,155 @@ bool wperf_version(PVERSION_INFO wperf_ver);
 /// <returns></returns>
 bool wperf_list_events(PLIST_CONF list_conf, PEVENT_INFO einfo);
 
+// With this example "-e inst_spec,dp_spec,{ld_spec,st_spec} -m dcache",
+// normal events are inst_spec and dp_spec,
+// group events are {ld_spec,st_spec},
+// metric is dcache.
+
+typedef enum _EVT_NOTE_TYPE
+{
+    NORMAL_EVT_NOTE,
+    GROUP_EVT_NOTE,
+    METRIC_EVT_NOTE
+} EVT_NOTE_TYPE;
+
+typedef struct _GROUP_NOTE
+{
+    /// Group ID
+    uint16_t group_id;
+} GROUP_NOTE;
+
+typedef struct _METRIC_NOTE
+{
+    /// Group ID
+    uint16_t group_id;
+    /// Metric name
+    const wchar_t* name;
+} METRIC_NOTE;
+
+typedef struct _EVENT_NOTE
+{
+    /// Event type
+    EVT_NOTE_TYPE type;
+    /// Depending on the event type, this field could be one of the following types.
+    union
+    {
+        GROUP_NOTE group_note;
+        METRIC_NOTE metric_note;
+    } note;
+} EVENT_NOTE;
+
+typedef struct _GROUP_EVENT_INFO
+{
+    /// The number of event groups to count.
+    /// With "-e {ld_spec,st_spec}, this value is 1.
+    int num_groups;
+    /// This array holds the number of events for each group.
+    /// With "-e {ld_spec,st_spec}, this array has only 1 element and looks like {2}.
+    int* num_group_events;
+    /// This 2-dimensional array holds the list of events for each group.
+    /// With "-e {ld_spec,st_spec}, this array looks like {{0x70 (ID of ld_spec), 0x71 (ID of st_spec)}}.
+    uint16_t** events;
+} GROUP_EVENT_INFO;
+
+typedef struct _STAT_CONF
+{
+    /// The number of cores to count on.
+    /// With "-c 0,3", this value is 2.
+    int num_cores;
+    /// The list of cores to count on.
+    /// With "-c 0,3", this array looks like {0, 3}).
+    uint8_t* cores;
+    /// The number of normal events to count.
+    /// With "-e inst_spec,dp_spec", this value is 2).
+    int num_events;
+    /// The list of normal events to count.
+    /// With "-e inst_spec,dp_spec", this array looks like {0x1B (ID of inst_spec), 0x73 (ID of dp_spec)}.
+    uint16_t* events;
+    /// The group events to count.
+    /// Refer to definition of GROUP_EVENT_INFO for more details.
+    GROUP_EVENT_INFO group_events;
+    /// The number of metrics to count.
+    /// With "-m dcache", this value is 1.
+    int num_metrics;
+    /// The list of metrics to count.
+    /// With "-m dcache", this looks like {"dcache"}.
+    const wchar_t** metric_events;
+    /// The counting duration (in seconds, accurary of 0.1 sec).
+    double duration;
+    /// Set this to true if kernel mode should be included, false if not.
+    bool kernel_mode;
+} STAT_CONF, *PSTAT_CONF;
+
+typedef struct _STAT_INFO
+{
+    /// Core on which the event was counted.
+    uint8_t core_idx;
+    /// Counter value.
+    uint64_t counter_value;
+    /// Event ID.
+    uint16_t event_idx;
+    /// Time slots scheduled.
+    uint64_t multiplexed_scheduled;
+    /// Total time slots.
+    uint64_t multiplexed_round;
+    /// Scaled counter value.
+    uint64_t scaled_value;
+    /// Event note, refer to definition of EVENT_NOTE for details.
+    EVENT_NOTE evt_note;
+} STAT_INFO, *PSTAT_INFO;
+
+/// <summary>
+/// Works like a generator, yields the next STAT_INFO from the list of all STAT_INFOs
+/// each time it's called event by event for all requested cores.
+/// </summary>
+/// <example> This example shows how to call the wperf_stat routine.
+/// <code>
+/// wperf_init();
+///
+/// uint8_t cores[2] = { 0, 3 };
+/// uint16_t events[2] = { 0x1B, 0x73 };
+/// int num_group_events[1] = { 2 };
+/// uint16_t group_events[2] = { 0x70, 0x71 };
+/// uint16_t* gevents[1] = { group_events };
+/// const wchar_t* metric_events[1] = { L"dcache" };
+/// STAT_CONF stat_conf =
+/// {
+///   2, // num_cores
+///   cores, // cores
+///   2, // num_events
+///   events, // events
+///   {1/*num_groups*/, num_group_events/*num_group_events*/, gevents/*events*/}, // group_events
+///   1, // num_metrics
+///   metric_events, // metric_events
+///   1, // duration
+///   false // kernel_mode
+/// }
+///
+/// if (wperf_stat(&stat_conf, NULL))
+/// {
+///   STAT_INFO stat_info;
+///   while (wperf_stat(&stat_conf, &stat_info))
+///   {
+///     printf("core_idx=%u, event_idx=%u, counter=%llu\n", stat_info.core_idx, stat_info.event_idx, stat_info.counter_value);
+///   }
+/// }
+///
+/// wperf_close();
+/// </code>
+/// </example>
+/// <param name="stat_conf">Pointer to a caller-allocated STAT_CONF struct. User configures
+/// the various parameters of the counting mode through setting the fields in stat_conf
+/// (refer to the definition of STAT_CONF for more details).</param>
+/// <param name="stat_info">Setting stat_info to NULL, this lib routine will collect
+/// counter values for all the requested events internally and be ready for subsequent calls
+/// to yield counter values event by event for all requestd cores. When passed as a pointer
+/// to a caller-allocated STAT_INFO structure. The lib routine will populate the STAT_INFO
+/// pointed to by stat_info with the counter values and other related information defined
+/// in STAT_INFO for each requested event on each requestd core.</param>
+/// <returns></returns>
+bool wperf_stat(PSTAT_CONF stat_conf, PSTAT_INFO stat_info);
+
 #ifdef __cplusplus
 }
 #endif
