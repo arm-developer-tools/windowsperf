146ddecfa338bc1ff5cd670b7eacce5cebaf4fda
Przemyslaw Wirkus <przemyslaw.wirkus@linaro.org>
2024-01-25 16:29:04 +0000

    [wperf-scripts] WPERF-670: Add basic sampling testing for CPython local native builds
    See merge request Linaro/WindowsPerf/windowsperf!488
    
    === ChangeLog ===
    
    * wperf-scripts: add _test suffix to wperf_cli_cpython_dep_record.py
    
    * wperf-scripts: fix name typo
    
    * wperf-scripts: generalize test_cpython_bench_record_hotspot test case
    
    * wperf-scripts: do not include cpython dir pytests
    
    * wperf-scripts: add MVP for CPython sampling testing
    
    * wperf-scrtips: added new module cpython with sha 1ff81c0cb67215694f084e51c4d35ae53b9f5cf9

---
+++ wperf-scripts/tests/wperf_cli_cpython_bench_test.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# BSD 3-Clause License
+#
+# Copyright (c) 2024, Arm Limited
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Module is testing `wperf sample` and `wperf record` with native CPython,
+   built from sourced debug executables.
+"""
+import os
+import pytest
+from common import run_command
+from common_cpython import CPYTHON_DIR, CPYTHON_EXE_DIR
+
+
+### Test cases
+
+def test_cpython_bench_build__deps():
+    """ Check if we have enough dependencies to run the CPython build."""
+    assert os.path.isdir(CPYTHON_DIR), f"submodule with CPython source-code '{CPYTHON_DIR}' doesn't exist"
+
+def test_cpython_bench_build__make():
+    """ Build CPython using its own build script. """
+
+    ### Build CPython for this platform, with debug on (-d) and for ARM64 platform (-p ARM64)
+    build_bat_path = os.path.join(CPYTHON_DIR, "build.bat")
+    cmd = f"{build_bat_path} -d -p ARM64"
+    stdout, _ = run_command(cmd)   # Build all tests
+
+    # Build sanity checks:
+    """
+    Build succeeded.
+    0 Warning(s)
+    0 Error(s)
+
+    Time Elapsed 00:00:58.42
+    """
+
+    # Sanity checks for the build success
+    assert b"Build succeeded." in stdout, "CPython build script failed"
+    assert b"0 Error(s)" in stdout, "CPython build script failed with error(s)"
+    assert os.path.isdir(CPYTHON_EXE_DIR), "CPython ARM64 build directory {CPYTHON_EXE_DIR} doesn't exist"
+
+    # Check for expected build output files
+    build_artifacts = ["python_d.exe", "python_d.pdb"]
+    for f in build_artifacts:
+        p = os.path.join(CPYTHON_EXE_DIR, f)
+        assert os.path.isfile(p)
---
+++ wperf-scripts/tests/wperf_cli_cpython_dep_record_test.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+# BSD 3-Clause License
+#
+# Copyright (c) 2024, Arm Limited
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Module is testing `wperf record` with CPython executables in debug mode."""
+import os
+import json
+import pytest
+from common import run_command, is_json, check_if_file_exists
+from common_cpython import CPYTHON_EXE_DIR
+
+
+### Test cases
+
+@pytest.mark.parametrize("EVENT,EVENT_FREQ,HOT_SYMBOL,HOT_MINIMUM,PYTHON_ARG",
+[
+    ("ld_spec", 10000, "x_mul:python312_d.dll", 70, "10**10**100"),
+]
+)
+def test_cpython_bench_record_hotspot(EVENT,EVENT_FREQ,HOT_SYMBOL,HOT_MINIMUM,PYTHON_ARG):
+    """ Test `wperf record` for python_d.exe call for example `Googolplex` calculation.
+        We will sample for one event + sampling frequency and we expect one hottest symbol
+        with some minimum sampling %."""
+
+    ## Execute benchmark
+    pyhton_d_exe_path = os.path.join(CPYTHON_EXE_DIR, "python_d.exe")
+
+    if not check_if_file_exists(pyhton_d_exe_path):
+        pytest.skip(f"Can't locate CPython native executable in {pyhton_d_exe_path}")
+
+    cmd = f"wperf record -e {EVENT}:{EVENT_FREQ} -c 7 --timeout 5 --json -- {pyhton_d_exe_path} -c {PYTHON_ARG}"
+    stdout, _ = run_command(cmd)
+
+    # Sanity checks
+    assert is_json(stdout), f"in {cmd}"
+    json_output = json.loads(stdout)
+
+    # Check sampling JSON output for expected functions
+    assert "sampling" in json_output
+    assert "events" in json_output["sampling"]
+
+    events = json_output["sampling"]["events"]  # List of dictionaries, for each event
+    assert len(events) == 1  # We expect one record for e.g. `ld_spec`
+
+    evt = json_output["sampling"]["events"][0]  # e.g. `ld_spec` data
+    assert EVENT in evt["type"]
+
+    samples = evt["samples"]
+
+    def find_sample(samples, symbol):
+        """ Find sample in samples and return it when we can find it for given `symbol`.
+        See:
+
+        "events": [
+            {
+                "type": "ld_spec",
+                "samples": [
+                    {
+                        "overhead": 81.6406,
+                        "count": 418,
+                        "symbol": "x_mul:python312_d.dll"
+                    },
+                    {
+                        "overhead": 4.88281,
+                        "count": 25,
+                        "symbol": "v_iadd:python312_d.dll"
+                    },
+        """
+        for sample in samples:
+            if sample["symbol"] == symbol:
+                return sample
+        return False
+
+    # {
+    #     "overhead": 81.6406,
+    #     "count": 418,
+    #     "symbol": "x_mul:python312_d.dll"
+    # },
+
+    evt_sample = find_sample(samples, HOT_SYMBOL)
+    assert evt_sample is not False, "Can't find `{HOT_SYMBOL}` symbol in sampling output!"
+    # We want to see at least e.g. 70% of samples in e.g `x_mul`:
+    assert evt_sample["overhead"] > HOT_MINIMUM, f"expected {HOT_MINIMUM}% sampling hotspot in {HOT_SYMBOL}"
