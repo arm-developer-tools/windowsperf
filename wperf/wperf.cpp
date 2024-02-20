30bd838c95a21b641e593d8a8b790a612c09c78b
Matthew Sykes <matthew.sykes@live.co.uk>
2024-02-20 13:05:55 +0000

    add more enumeration code

--- wperf/wperf.cpp
+++ wperf/wperf.cpp
@@ -99,10 +99,72 @@ GetDevicePath(
         goto clean0;
     }
 
+    PWSTR deviceInterface;
+
+    for (deviceInterface = deviceInterfaceList; *deviceInterface; deviceInterface += wcslen(deviceInterface) + 1)
+    {
+        DEVPROPTYPE devicePropertyType;
+        ULONG deviceHWIdListLength = 256;
+        WCHAR deviceHWIdList[256];
+        CONFIGRET cr1 = CR_SUCCESS;
+
+        WindowsPerfDbgPrint("Found device  %S\n\n", deviceInterface);
+
+        ULONG deviceInstanceIdLength = MAX_DEVICE_ID_LEN;
+        WCHAR deviceInstanceId[MAX_DEVICE_ID_LEN];
+
+        cr1 = CM_Get_Device_Interface_Property(
+            deviceInterface,
+            &DEVPKEY_Device_InstanceId,
+            &devicePropertyType,
+            (PBYTE)deviceInstanceId,
+            &deviceInstanceIdLength,
+            0);
+        if (cr1 != CR_SUCCESS)
+        {
+            UINT err = GetLastError();
+            WindowsPerfDbgPrint("CM_Get_Device_Interface_Property DEVPKEY_Device_InstanceId failed with 0x%X %d\n\n", cr1, err);
+            continue;
+        }
+        WindowsPerfDbgPrint("Found instance %S\n\n", deviceInstanceId);
+
+        DEVINST deviceInstanceHandle;
+
+        cr1 = CM_Locate_DevNode(&deviceInstanceHandle, deviceInstanceId, CM_LOCATE_DEVNODE_NORMAL);
+        if (cr1 != CR_SUCCESS)
+        {
+            UINT err = GetLastError();
+            WindowsPerfDbgPrint("CM_Locate_DevNode  failed with 0x%X %d\n\n", cr1, err);
+            continue;
+        }
+
+        cr1 = CM_Get_DevNode_Property(
+            deviceInstanceHandle,
+            &DEVPKEY_Device_HardwareIds,
+            &devicePropertyType,
+            (PBYTE)deviceHWIdList,
+            &deviceHWIdListLength,
+            0);
+        if(cr1  != CR_SUCCESS)
+        {
+            UINT err = GetLastError();
+            WindowsPerfDbgPrint("CM_Get_Device_Interface_Property DEVPKEY_Device_HardwareIds failed with 0x%X %d\n\n", cr1, err);
+            continue;
+        }
+
+        // hardware IDs is a null sperated list of strings, 
+        PWSTR hdId;
+        for (hdId = deviceHWIdList; *hdId; hdId += wcslen(hdId) + 1)
+        {
+            WindowsPerfDbgPrint("Found HW ID  %S\n\n", hdId);
+        }
+
+    }
+
     nextInterface = deviceInterfaceList + wcslen(deviceInterfaceList) + 1;
     if (*nextInterface != UNICODE_NULL) {
         WindowsPerfDbgPrint("Warning: More than one device interface instance found. \n"
-                            "         Selecting first matching device  %S.\n\n", nextInterface);
+                            "         Selecting first matching device.\n\n");
     }
 
     hr = StringCchCopy(DevicePath, BufLen, deviceInterfaceList);
