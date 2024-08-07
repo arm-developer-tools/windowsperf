0640ff3d742b18a3d2f4a28e154cf8295f105552
kush-kothari-arm <kush.kothari@arm.com>
2024-08-07 20:25:07 +0000

    [wperf] WPERF-947: `man.cpp` and `man.h` refactor
    See merge request Linaro/WindowsPerf/windowsperf!678
    
    === ChangeLog ===
    
    * fixed bug using .at() to handle std::out_of_range exception being returned rather than just 0
    
    * fixed invalid function references
    
    * made 2 functions man_ for completeness
    
    * reflect const pmu_device change in header
    
    * made references to pmu_device& const, changed map accesses to .at() to avoid read/write issues from op[]
    
    * removed static functions from headers, forward declared them within the .cpp
    
    * made more functions within man.cpp static
    
    * made Item --> man_Item
    
    * merged 'CPU' addition to print functions

--- wperf/man.cpp
+++ wperf/man.cpp
@@ -9,19 +9,26 @@
 #include <vector>
 #include <iostream>
 
-static std::vector<std::wstring> _col1;
-static std::vector<std::wstring> _col2;
+bool inline static man_is_valid_cpu(const pmu_device& pdev, std::wstring name);
+std::vector<std::wstring> static man_get_valid_cpu_list(const pmu_device& pdev);
+void static man_print_metric(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_metric);
+void static man_print_event(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_event);
+void static man_print_groups_metric(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_arg);
+bool static man_find_in_product_data(const pmu_device& pdev, std::wstring product_name, const std::wstring& requested_arg);
 
-struct Item 
+static std::vector<std::wstring> man_col1;
+static std::vector<std::wstring> man_col2;
+
+struct man_Item 
 {
 	enum Type {Event,Metric,Group_Metric} type;
 	std::wstring product;
 	std::wstring name;
 };
 
-static std::vector<Item> requested_items;
+static std::vector<man_Item> requested_items;
 
-bool inline static is_valid_cpu(const pmu_device& pdev, std::wstring name)
+bool inline static man_is_valid_cpu(const pmu_device& pdev, std::wstring name)
 {
 	std::vector<std::wstring> arm_arch = { pdev.m_PRODUCT_ARMV8A, pdev.m_PRODUCT_ARMV9A };
 	return pdev.m_product_configuration.count(name) > 0 ||
@@ -36,97 +43,109 @@ std::vector<std::wstring> static man_get_valid_cpu_list(const pmu_device& pdev)
 	return result;
 }
 
-void print_metric(pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_metric) 
+void static man_print_metric(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_metric)
 {
-	product_metric metric = pdev.m_product_metrics[product_name][requested_metric];
+	product_metric metric = pdev.m_product_metrics.at(product_name).at(requested_metric);
 
-	_col1.push_back(L"CPU");
-	_col2.push_back(product_name);
+	man_col1.push_back(L"CPU");
+	man_col2.push_back(product_name);
 
-	_col1.push_back(L"NAME");
-	_col2.push_back(metric.name + L" - " + metric.title);
+	man_col1.push_back(L"NAME");
+	man_col2.push_back(metric.name + L" - " + metric.title);
 
 	std::wstring formatted_events = metric.events_raw;
 	ReplaceAllTokensInWString(formatted_events, L",", L", ");
 
-	_col1.push_back(L"EVENTS");
-	_col2.push_back(formatted_events);
+	man_col1.push_back(L"EVENTS");
+	man_col2.push_back(formatted_events);
 
-	_col1.push_back(L"DESCRIPTION");
-	_col2.push_back(metric.description);
+	man_col1.push_back(L"DESCRIPTION");
+	man_col2.push_back(metric.description);
 
-	_col1.push_back(L"FORMULA");
-	_col2.push_back(metric.metric_formula);
-	_col1.push_back(L"UNIT");
-	_col2.push_back(metric.metric_unit);
+	man_col1.push_back(L"FORMULA");
+	man_col2.push_back(metric.metric_formula);
+	man_col1.push_back(L"UNIT");
+	man_col2.push_back(metric.metric_unit);
 }
 
 
-void print_event(pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_event) 
+void static man_print_event(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_event)
 {
-	product_event event = pdev.m_product_events[product_name][requested_event];
+	product_event event = pdev.m_product_events.at(product_name).at(requested_event);
 	
-	_col1.push_back(L"CPU");
-	_col2.push_back(product_name);
+	man_col1.push_back(L"CPU");
+	man_col2.push_back(product_name);
 
-	_col1.push_back(L"NAME");
-	_col2.push_back(event.name + L" - " + event.title);
+	man_col1.push_back(L"NAME");
+	man_col2.push_back(event.name + L" - " + event.title);
 
-	_col1.push_back(L"DESCRIPTION");
-	_col2.push_back(event.description);
+	man_col1.push_back(L"DESCRIPTION");
+	man_col2.push_back(event.description);
 }
 
-void print_groups_metric(pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_arg) 
+void static man_print_groups_metric(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_arg)
 {
-	product_group_metrics group_metrics = pdev.m_product_groups_metrics[product_name][requested_arg];
+	product_group_metrics group_metrics = pdev.m_product_groups_metrics.at(product_name).at(requested_arg);
 
-	_col1.push_back(L"CPU");
-	_col2.push_back(product_name);
+	man_col1.push_back(L"CPU");
+	man_col2.push_back(product_name);
 
-	_col1.push_back(L"NAME");
-	_col2.push_back(group_metrics.name + L" - " + group_metrics.title);
+	man_col1.push_back(L"NAME");
+	man_col2.push_back(group_metrics.name + L" - " + group_metrics.title);
 
-	_col1.push_back(L"DESCRIPTION");
-	_col2.push_back(group_metrics.description);
+	man_col1.push_back(L"DESCRIPTION");
+	man_col2.push_back(group_metrics.description);
 
 	std::wstring formatted_metrics = group_metrics.metrics_raw;
 	ReplaceAllTokensInWString(formatted_metrics, L",", L", ");
 
-	_col1.push_back(L"METRICS");
-	_col2.push_back(formatted_metrics);
+	man_col1.push_back(L"METRICS");
+	man_col2.push_back(formatted_metrics);
 }
 
-bool find_in_product_data(pmu_device& pdev, std::wstring product_name, const std::wstring& requested_arg)
-{	
-	if (pdev.is_alias_product_name(product_name))
-		product_name = pdev.get_product_name(product_name);
-
-	if (pdev.m_product_metrics[product_name].count(requested_arg) > 0)
-	{
-		requested_items.push_back({ Item::Metric, product_name, requested_arg });
-		return true;
-	}
-
-	else if (pdev.m_product_events[product_name].count(requested_arg) > 0) 
-	{
-		requested_items.push_back({ Item::Event, product_name, requested_arg });
-		return true;
-	}
-
-	else if (pdev.m_product_groups_metrics[product_name].count(requested_arg) > 0) 
-	{
-		requested_items.push_back({ Item::Group_Metric, product_name, requested_arg });
-		return true;
-	}
-
-	return false;
-}
-
-
-void man(pmu_device& pdev, std::vector<std::wstring>& args, const wchar_t& delim, const std::wstring& product_name, std::vector<std::wstring>& col1, std::vector<std::wstring>& col2) 
+bool static man_find_in_product_data(const pmu_device& pdev, std::wstring product_name, const std::wstring& requested_arg)
 {
-	_col1.clear();
-	_col2.clear();
+	try 
+	{
+		if (pdev.is_alias_product_name(product_name)) 
+		{
+			product_name = pdev.get_product_name(product_name);
+		}
+
+		const auto& metrics_map = pdev.m_product_metrics.at(product_name);
+		const auto& events_map = pdev.m_product_events.at(product_name);
+		const auto& group_metrics_map = pdev.m_product_groups_metrics.at(product_name);
+
+		if (metrics_map.find(requested_arg) != metrics_map.end()) 
+		{
+			requested_items.push_back({ man_Item::Metric, product_name, requested_arg });
+			return true;
+		}
+		else if (events_map.find(requested_arg) != events_map.end()) 
+		{
+			requested_items.push_back({ man_Item::Event, product_name, requested_arg });
+			return true;
+		}
+		else if (group_metrics_map.find(requested_arg) != group_metrics_map.end()) 
+		{
+			requested_items.push_back({ man_Item::Group_Metric, product_name, requested_arg });
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	catch (const std::out_of_range&) {
+		return false;
+	}
+}
+
+
+void man(const pmu_device& pdev, std::vector<std::wstring>& args, const wchar_t& delim, const std::wstring& product_name, std::vector<std::wstring>& col1, std::vector<std::wstring>& col2) 
+{
+	man_col1.clear();
+	man_col2.clear();
 
 	std::wstring detected_cpu = product_name;
 	std::wstring cur_cpu;
@@ -137,7 +156,6 @@ void man(pmu_device& pdev, std::vector<std::wstring>& args, const wchar_t& delim
 		m_out.GetErrorOutputStream() << L"warning: no argument(s) specified, specify an event, metric, or group of metrics to obtain relevant, manual style, information." << std::endl;
 		throw fatal_exception("ERROR_REQUESTED_ARG");
 	}
-
 
 	for (auto &arg : args)
 	{
@@ -152,7 +170,7 @@ void man(pmu_device& pdev, std::vector<std::wstring>& args, const wchar_t& delim
 			requested_arg = arg;
 		}
 
-		if (!is_valid_cpu(pdev, cur_cpu)) 
+		if (!man_is_valid_cpu(pdev, cur_cpu)) 
 		{
 			std::wstring avail_cpus = WStringJoin(man_get_valid_cpu_list(pdev), L", ");
 
@@ -161,7 +179,7 @@ void man(pmu_device& pdev, std::vector<std::wstring>& args, const wchar_t& delim
 		}
 
 		//find event,group or event_group for cur_cpu and print them
-		if (!find_in_product_data(pdev, cur_cpu, requested_arg))
+		if (!man_find_in_product_data(pdev, cur_cpu, requested_arg))
 		{
 			m_out.GetErrorOutputStream() << L"warning: \"" << requested_arg << L"\" not found! Ensure it is compatible with the specified CPU" << std::endl;
 			throw fatal_exception("ERROR_REQUESTED_ARG");
@@ -172,18 +190,18 @@ void man(pmu_device& pdev, std::vector<std::wstring>& args, const wchar_t& delim
 	{
 		switch (item.type) 
 		{
-			case Item::Event:
-				print_event(pdev, item.product, item.name);
+			case man_Item::Event:
+				man_print_event(pdev, item.product, item.name);
 				break;
-			case Item::Metric:
-				print_metric(pdev, item.product, item.name);
+			case man_Item::Metric:
+				man_print_metric(pdev, item.product, item.name);
 				break;
-			case Item::Group_Metric:
-				print_groups_metric(pdev, item.product, item.name);
+			case man_Item::Group_Metric:
+				man_print_groups_metric(pdev, item.product, item.name);
 				break;
 		}
 	}
 
-	col1 = _col1;
-	col2 = _col2;
+	col1 = man_col1;
+	col2 = man_col2;
 }
--- wperf/man.h
+++ wperf/man.h
@@ -5,8 +5,4 @@
 #include <vector>
 #include <iostream>
 
-void print_metric(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_metric);
-void print_event(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_event);
-void print_groups_metric(const pmu_device& pdev, const std::wstring& product_name, const std::wstring& requested_arg);
-bool find_in_product_data(pmu_device& pdev, std::wstring product_name, const std::wstring& requested_arg);
-void man(pmu_device& pdev, std::vector<std::wstring>& token_args, const wchar_t& delim, const std::wstring& product_name, std::vector<std::wstring>& col1, std::vector<std::wstring>& col2);
+void man(const pmu_device& pdev, std::vector<std::wstring>& token_args, const wchar_t& delim, const std::wstring& product_name, std::vector<std::wstring>& col1, std::vector<std::wstring>& col2);
